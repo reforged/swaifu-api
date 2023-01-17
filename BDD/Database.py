8b3e86034eaa9edeec3ae59d816409832fe31fc9
Patasc <45915005+Patasc@users.noreply.github.com>
2023-01-17 12:51:27 +0100

    :construction: Implémentation des bases pour les requêtes :construction:
    Ajouts de fonctions nécessaires pour la bonne gestion des base de données, notamment d'un parseur

--- BDD/Database.py
+++ BDD/Database.py
@@ -2,6 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class Database(ABC):
+    """
+    Classe abstraite afin d'offrir une interface commune à tous les types de systèmes de gestion de données
+    Notamment base de donnée MySQL ou un système de fichier
+    """
     @abstractmethod
     def query(self, request):
         pass

9205b6d39f3e5cdea7ff801cd9eb5176a859e2e8
Patasc <45915005+Patasc@users.noreply.github.com>
2023-01-22 17:15:44 +0100

    :bookmark: Nettoyage post-merge

--- BDD/ConnectionHandler.py
+++ BDD/ConnectionHandler.py
@@ -1,8 +1,9 @@
 from BDD.Database import Database
 from BDD.BDD_PSQL.PsqlDatabase import PsqlDatabase
+from BDD.BDD_Fichiers.FichierDatabase import RequetesFormatTxt
 from Erreurs.BDD import BDDNonPriseEnCharge
 
-handlers = {"fichier": None, "psql": PsqlDatabase}
+handlers = {"fichier": RequetesFormatTxt, "psql": PsqlDatabase}
 
 
 def initiate(systeme: str, params: dict[str, str]) -> Database:
